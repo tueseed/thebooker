029d6a68e3b920e2d014ebb12af60219383e21e1
tueseed <tue_seed@hotmail.com>
2021-01-02 15:05:03 +0700

    mkomoviav

--- scripts/global.js
+++ scripts/global.js
@@ -22,7 +22,7 @@ function processLogin()
   {
     window.location.href= 'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=' + chennelId + '&redirect_uri=' + callBackurl + '&state=12345abcd&scope=openid%20profile'
   }
-  else if(ocalStorage.getItem('name') != null)
+  else if(localStorage.getItem('name') != null)
   {
     var code = getUrlVars()["code"]//ตัวแปร code จะได้มาก็ต่อเมื่อ Login ผ่านแล้ว เอาค่าของตัวแปร code ไปแลกข้อมูล
     $.ajax({
