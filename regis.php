700582c20a1d589b5f2d28641d47348cb6d64232
tueseed <tue_seed@hotmail.com>
2021-01-03 12:41:42 +0700

    อ่รนกอ่รีฟกิฟ

--- regis.php
+++ regis.php
@@ -8,22 +8,14 @@
     <div class="col-lg-4 offset-lg-4">
         <div class="form-group">
             <label for="nameInput" class="font-weight-bold text-white"><i class="fas fa-user-edit"></i> ชื่อ - นามสกุล</label>
-            <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="ชื่อ - นามสกุลของท่าน" required>
+            <input type="text" class="form-control" id="membername"  >
         </div>
         <div class="form-group">
-            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> ตำแหน่ง</label>
+            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> ชั้นเรียน/หน่วยงาน</label>
             <input type="text" class="form-control" id="position" name="position" placeholder="กรอกตำแหน่ง/สังกัด" required>
         </div>
         <div class="form-group">
-            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> แผนก</label>
-            <select class="form-control" id="secTion">
-                <option value="cn">แผนกก่อสร้าง</option>
-                <option value="cs">แผนกบริการลูกค้า</option>
-                <option value="om">แผนกปฏิบัติการและบำรุงรักษา</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> รหัสพนักงาน/</label>
+            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> รหัสนักเรียน/รหัสพนักงาน</label>
             <input type="text" class="form-control" id="staffId" name="staffId" placeholder="กรอกรหัสพนักงาน" required>
         </div>
         <button type="ิbutton" class="btn btn-primary" onclick="regis_emp()">ส่งข้อมูล</button>
