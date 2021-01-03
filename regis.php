a536f63e386e3ddfa7336f19f5e1ec820d9da181
tueseed <tue_seed@hotmail.com>
2021-01-03 12:29:34 +0700

    vjifojvi9

--- regis.php
+++ regis.php
@@ -4,52 +4,28 @@
     </div>
 </div>
 
-<div class="row ml-2 mr-2">
-    <div class="col-lg-12">
-    <div class="card shadow" id="cardaddbook">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="form-group">
-                <label for="nameInput" class="font-weight-bold text-dark"><i class="fas fa-book-medical"></i> ชื่อหนังสือ</label>
-                <input type="text" class="form-control" id="bookname" name="nameInput" placeholder="ตัวอย่าง วิทยาศาสตร์ ม.1" required>
-            </div>
-            <div class="form-group">
-                <label for="nameInput" class="font-weight-bold text-dark"><i class="fas fa-user-edit"></i> ผู้เขียน</label>
-                <input type="text" class="form-control" id="writer" name="nameInput" placeholder="ตัวอย่าง Mr.science" required>
-            </div>
-            <div class="form-group">
-                <label for="nameInput" class="font-weight-bold text-dark"><i class="fas fa-list-alt"></i> หมวดหมู่</label>
-                <input type="text" class="form-control" id="category" name="category" placeholder="ตัวอย่าง วิทยาศาสตร์ ศิลป" required>
-            </div>
-            <div class="form-group">
-                <label for="nameInput" class="font-weight-bold text-dark"><i class="fas fa-images" ></i> รูปภาพหน้าปก</label>
-                <input type="text" class="form-control" id="bookcoverimage" name="nameInput" onkeyup="reqpicurl()" placeholder="ตัวอย่าง นายนัทธพงศ์ เจริญกิจพิเชียร" required>
-            </div>
-            
-          </div>
-          <div class="col-lg-4">
-            <div class="form-group">
-                <label for="nameInput" class="font-weight-bold text-dark"><i class="fas fa-indent"></i> เรื่องย่อ</label>
-                <textarea class="form-control" id="bookdescription" rows="12"></textarea>
-            </div>
-          </div>
-          <div class="col-lg-4 text-center">
-                <div class="img-square-wrapper">
-                    <img class="" id="bookcover" src="images/bookcover.jpg" onError="this.onerror=null;this.src='images/bookcover.jpg';" width="275" height="340" alt="Card image cap">
-                </div>
-          </div>
-          
+<div class="row text-center">
+    <div class="col-lg-4 offset-lg-4">
+        <div class="form-group">
+            <label for="nameInput" class="font-weight-bold text-white"><i class="fas fa-user-edit"></i> ชื่อ - นามสกุล</label>
+            <input type="text" class="form-control" id="nameInput" name="nameInput" placeholder="ชื่อ - นามสกุลของท่าน" required>
         </div>
-
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="form-group">
-              <button type="ิbutton" class="btn btn-block mx-auto shadow" style = "background-color: #DDB892;" onclick="addbook()"><i class="fas fa-check-circle"></i>  ส่งข้อมูล</button>
-            </div>
-          </div>
+        <div class="form-group">
+            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> ตำแหน่ง</label>
+            <input type="text" class="form-control" id="position" name="position" placeholder="กรอกตำแหน่ง/สังกัด" required>
         </div>
-      </div>
-  </div>
+        <div class="form-group">
+            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> แผนก</label>
+            <select class="form-control" id="secTion">
+                <option value="cn">แผนกก่อสร้าง</option>
+                <option value="cs">แผนกบริการลูกค้า</option>
+                <option value="om">แผนกปฏิบัติการและบำรุงรักษา</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="telInput" class="font-weight-bold text-white"><i class="fa fa-address-card"></i> รหัสพนักงาน/</label>
+            <input type="text" class="form-control" id="staffId" name="staffId" placeholder="กรอกรหัสพนักงาน" required>
+        </div>
+        <button type="ิbutton" class="btn btn-primary" onclick="regis_emp()">ส่งข้อมูล</button>
     </div>
 </div>
