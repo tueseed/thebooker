93d8baadd28995d8e03a3f68498fd0ce5ca99b6a
tueseed <tue_seed@hotmail.com>
2021-01-18 09:21:23 +0700

    jgifojavio-sag

--- mobile.php
+++ mobile.php
@@ -1,8 +1,10 @@
 <div class="row ml-2 mr-2">
+  <div class="col-lg-12">
   <div class="input-group mb-3">
     <div class="input-group-prepend">
       <span class="input-group-text" id="basic-addon1">ค้นหาหนังสือ</span>
     </div>
     <input type="text" class="form-control" placeholder="พิมพ์ชื่อหนังสือ,ผู้เขียน,หมวดหมู่ เพื่อค้นหา" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
   </div>
 </div>
