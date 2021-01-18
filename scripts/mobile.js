95ed4a2343b7d82e3537878de3962a9bc34a4172
tueseed <tue_seed@hotmail.com>
2021-01-18 10:31:27 +0700

    fjdiouhjapvdf

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,4 +1,14 @@
 $('#header').hide()
+liff
+  .init({
+    liffId: "1655539437-ploBZxNG" // Use own liffId
+  })
+  .then(() => {
+    liff.getProfile().then(profile => {
+                                        const name = profile.displayName
+                                        alert(name)
+                            })
+  })
 $(document).ready(
     function(){
                 querybook()
