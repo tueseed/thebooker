abfcdf09a7c8ac4ee032ffd2b50cb309f95bcd15
tueseed <tue_seed@hotmail.com>
2021-01-18 09:09:36 +0700

    nvjfoipaviuad[fv

--- home.php
+++ home.php
@@ -1,9 +1,11 @@
 <div class="row ml-2 mr-2">
-  <div class="input-group mb-3">
-    <div class="input-group-prepend">
-      <span class="input-group-text" id="basic-addon1">ค้นหาหนังสือ</span>
+  <div class="col-lg-12">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">ค้นหาหนังสือ</span>
+      </div>
+      <input type="text" class="form-control" placeholder="พิมพ์ชื่อหนังสือ,ผู้เขียน,หมวดหมู่ เพื่อค้นหา" aria-label="Username" aria-describedby="basic-addon1">
     </div>
-    <input type="text" class="form-control" placeholder="พิมพ์ชื่อหนังสือ,ผู้เขียน,หมวดหมู่ เพื่อค้นหา" aria-label="Username" aria-describedby="basic-addon1">
   </div>
 </div>
 <div class="row ml-2 mr-2" id="showbook">
