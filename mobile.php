3d184a24b882646b7b8e603c98a451751f0fcb4e
tueseed <tue_seed@hotmail.com>
2021-01-18 09:59:38 +0700

    mkofjvbsab

--- mobile.php
+++ mobile.php
@@ -8,6 +8,22 @@
         <input type="text" class="form-control" placeholder="พิมพ์ชื่อหนังสือ,ผู้เขียน,หมวดหมู่ เพื่อค้นหา" aria-label="Username" aria-describedby="basic-addon1">
       </div>
     </div>
+    <div class="row" id="bookmobile">
+      <!-- <div class="card shadow mt-2">
+        <div class="card-vertiocal">
+          <div class="img-square-wrapper shadow tex-center">
+              <img class="" src="https://lh3.googleusercontent.com/E_nfV7z6GqS77EiOlKDjXMGP553GudVmV_HjWTrrF96ac6opNdt_1dKa8n7D2ZUGHkq004mtLhOoS_z-AtKMvcWnunYZ9JZsB4GmnYCrLzunzxyyu0BsASVjgM_WNpqj3qyyPMc4=w2400" width="150" height="215" alt="Card image cap">
+          </div>
+          <div class="card-body">
+              <h6 class="card-title">ชื่อหนังสือ : Python For Education</h4>
+              <p class="card-texts">ผู้เขียน : ใครคนนั้น</p>
+              <p class="card-text">หมวดหมู่ : คอมพิวเตอร์ โปรแกรม เทคโนโลยี</p>
+              <p class="card-text">สถานะ : ว่าง</p>
+              <p class="card-text">กำหนดคืน : --</p>
+          </div>
+        </div>
+      </div> -->
+    </div>
   </div>
 
-  
+
--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,1 +1,77 @@
 $('#header').hide()
+
+function querybook()
+{
+    var formData = new FormData()
+  formData.append('command','allbook')
+  $.ajax({
+            url: 'api/api_book_all.php',
+            method: 'POST',
+            data:formData,
+            async: true,
+            cache: false,
+            processData: false,
+            contentType: false,
+            beforeSend : function()
+            {  
+                $('#bookmobile').block({
+                                        message: '<div class="spinner-border text-primary display-4" style="width: 4rem; height: 4rem;" role="status"><span class="sr-only">Loading...</span></div>',
+                                        overlayCSS : { 
+                                                        backgroundColor: '#ffffff',
+                                                        opacity: 1
+                                                    },
+                                        css : {
+                                            opacity: 1,
+                                            border: 'none',
+                                        }
+                                        })
+            },
+            success: function(response) 
+            {
+                var obj = JSON.parse(response)
+                var j =0;
+                while(obj[j])
+                {
+                    $('#bookmobile').append(makebookcard(obj[j]))
+                    j++;
+                }
+                console.log(obj)
+                // console.log(response)
+                // location.reload()      
+            },
+            complete :function()
+            {
+                $('#bookmobile').unblock()    
+            }					
+        })
+}
+
+function makebookcard(bookdetail)
+{
+  if(bookdetail.status == '0')
+  {
+    var bookstatustext = '<span class="card-text text-success">ว่าง</span>'
+  }
+  else if(bookdetail.status == '1')
+  {
+    var bookstatustext = '<span class="card-text text-danger">ไม่ว่าง</span>'
+  }
+  return[
+    '<div class="col-lg-5 mt-3">',
+    '<a class="card nav-link shadow" href="?action=bookdetail&bookid=' + bookdetail.bookid + '">',
+      '<div class="card-vertical">',
+        '<div class="img-square-wrapper shadow text-center">',
+            '<img class="" src="' + bookdetail.coverimage+ '" width="150" height="215" alt="Card image cap">',
+        '</div>',
+        '<div class="card-body">',
+            '<h5 class="card-title">ชื่อหนังสือ : ' + bookdetail.bookname + '</h4>',
+            '<p class="card-texts">ผู้เขียน : ' + bookdetail.writer + '</p>',
+            '<p class="card-text">หมวดหมู่ : ' + bookdetail.category + '</p>',
+            '<p class="card-text">สถานะ : ' + bookstatustext+ '</p>',
+            '<p class="card-text">กำหนดคืน : ' +bookdetail.datereturn + '</p>',
+        '</div>',
+      '</div>',
+    '</a>',
+  '</div>'
+  ].join("")
+}
