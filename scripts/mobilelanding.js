1e2da9363b88ac5d1fc7405a3dc5235ddb209fa9
tueseed <tue_seed@hotmail.com>
2021-01-20 09:27:09 +0700

    vkfpojvasdfb

--- scripts/mobilelanding.js
+++ scripts/mobilelanding.js
@@ -1,2 +1,12 @@
 $('#header').hide()
-window.location.href = "index.php?action=mobile&cache=" + new Date().getTime();
+liff
+  .init({
+    liffId: "1655539437-ploBZxNG" // Use own liffId
+  })
+  .then(() => {
+    liff.getProfile().then(profile => {
+                                        const name = profile.displayName
+                                        alert(name)
+                                        window.location.href = "index.php?action=mobile&cache=" + new Date().getTime();
+                            })
+  })
