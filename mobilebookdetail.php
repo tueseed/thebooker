a127075c7cc50b0989aa4c8345a1009c10ee6142
tueseed <tue_seed@hotmail.com>
2021-01-20 08:14:03 +0700

    jiojvfadv

---
+++ mobilebookdetail.php
@@ -0,0 +1,8 @@
+<div class="col-lg-12 mt-3 mb-3">
+    <div class="row">
+    <div class="img-square-wrapper shadow tex-center">
+        <img class="" src="https://lh3.googleusercontent.com/E_nfV7z6GqS77EiOlKDjXMGP553GudVmV_HjWTrrF96ac6opNdt_1dKa8n7D2ZUGHkq004mtLhOoS_z-AtKMvcWnunYZ9JZsB4GmnYCrLzunzxyyu0BsASVjgM_WNpqj3qyyPMc4=w2400" width="150" height="215" alt="Card image cap">
+    </div>
+    </div>
+   
+  </div>
