a22b33aee927909c31aacacb8d2d86aafce30d4b
tueseed <tue_seed@hotmail.com>
2021-02-02 14:10:33 +0700

    nxjihxasc

--- scripts/mobilebookdetail.js
+++ scripts/mobilebookdetail.js
@@ -6,8 +6,8 @@ liff
   .then(() => {
     liff.getProfile().then(profile => {
                                         const name = profile.displayName
-                                        // const uid = profile.uid
-                                        alert(name)
+                                        const uid = profile.uid
+                                        alert(uid)
                             })
   })
 querybookdetailformobile()
