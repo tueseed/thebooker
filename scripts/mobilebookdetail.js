ab084472e58e6608610f65ceae8c5d0f2996dc11
tueseed <tue_seed@hotmail.com>
2021-02-07 14:17:11 +0700

    mvkfo[jvdv

--- scripts/mobilebookdetail.js
+++ scripts/mobilebookdetail.js
@@ -101,14 +101,14 @@ function putin()
                 success: function(response) 
                 {
                     alert(response) 
-                    if(response == 0)
+                    if(response == '0')
                     {
                         Swal.fire({
                         title: "หยิบหนังสือใส่ตระกร้าแล้ว",
                         type: 'success' 
                         }).then(location.reload())
                     }
-                    else if(response == 0)
+                    else if(response == '1')
                     {
                         Swal.fire({
                             title: "หนังสือเล่มนี้อยู่ในตระกร้าแล้ว",
