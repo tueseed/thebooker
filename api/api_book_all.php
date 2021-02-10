387912a85f569606a0b470d8d7be2288ea4cd584
tueseed <tue_seed@hotmail.com>
2021-02-10 19:04:39 +0700

    cdsacf

--- api/api_book_all.php
+++ api/api_book_all.php
@@ -66,31 +66,38 @@
             
             $putinsqltext = "INSERT INTO tbl_borrow(uid,book_id,date,bill_id) VALUES('$uid','$bookid','$date','$lastid')";
             mysqli_query($conn,$putinsqltext);
+            echo '0';
         }
         else if($obj_check["last_bill"] !== null)
         {
             $last_bill = $obj_check["last_bill"];
-            echo $last_bill;
+            $checkbook = "SELECT * FROM tbl_borrow WHERE book_id='$bookid' AND bill_id='$last_bill";
+            $result = mysqli_query($conn,$checkbook);
+            $num = mysqli_num_rows($result);
+            if($num == 0)
+            {
+                $putinsqltext = "INSERT INTO tbl_borrow(uid,book_id,date) VALUES('$uid','$bookid','$date')";
+                mysqli_query($conn,$putinsqltext);
+                echo '0';
+            }
+            else if($num > 0)
+            {
+                echo '1';
+            }
         }
-        // $checkbook = "SELECT * FROM tbl_borrow WHERE book_id='$bookid'";
-        // $result = mysqli_query($conn,$checkbook);
-        // $num = mysqli_num_rows($result);
-        // if($num == 0)
-        // {
-        //     $putinsqltext = "INSERT INTO tbl_borrow(uid,book_id,date) VALUES('$uid','$bookid','$date')";
-        //     mysqli_query($conn,$putinsqltext);
-        //     echo '0';
-        // }
-        // else if($num > 0)
-        // {
-        //     echo '1';
-        // }
+        
         
     }
     else if($cmd == 'checkbasket')
     {
         $uid = $_POST["uid"];
-        $queryallbook = "SELECT * FROM tbl_borrow INNER JOIN tbl_book ON tbl_borrow.book_id=tbl_book.bookid WHERE uid='$uid'";
+
+        $mysql_check_last_bill = "SELECT MAX(bill_id) AS last_bill FROM tbl_bill WHERE uid ='$uid' AND bill_status='0'";
+        $query_check = mysqli_query($conn,$mysql_check_last_bill);
+        $obj_check = mysqli_fetch_assoc($query_check);
+        $last_bill = $obj_check["last_boill"];
+
+        $queryallbook = "SELECT * FROM tbl_borrow INNER JOIN tbl_book ON tbl_borrow.book_id=tbl_book.bookid WHERE uid='$uid' AND bill_id='$last_bill'";
         $query = mysqli_query($conn,$queryallbook);
         $data = array();
         while($obj = mysqli_fetch_assoc($query))
