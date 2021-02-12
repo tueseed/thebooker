613215875c696e872cee37405955c01a2a61372b
tueseed <tue_seed@hotmail.com>
2021-02-12 10:41:04 +0700

    jkopikbsh

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -8,12 +8,12 @@ liff
                                         const name = profile.displayName
                                         const uid = profile.userId
                                         sessionStorage.setItem('uid',uid)
-                                            
+                                        checkregis()  
                             })
   })
 $(document).ready(
     function(){
-      checkregis()
+      
       querybook()
               }
 )
